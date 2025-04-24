e7d9014e21d5fbe3d7e9b17d4071bbe75f6a435b
hoang nguyen <hoangdinh977@gmail.com>
2025-04-24 16:29:25 +0700

    again

--- RMS/Program.cs
+++ RMS/Program.cs
@@ -54,7 +54,8 @@ namespace RMS
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddScoped<IDiscountService, DiscountService>();
-
+            builder.Services.AddScoped<IBillService, BillService>();
+            
             // Add these services in Program.cs
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
